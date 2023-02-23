d204e28fc6385a3e1bdad1c6646e620b0a96c7c4
Nick Markovicz <hh.markovich.n@gmail.com>
2023-02-23 13:38:47 +0300

    Fixed 'profiles_view' view

--- shop/profiles/views.py
+++ shop/profiles/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 import logging
 from django.http import HttpResponse
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 
 
-def profiles_index(request):
-    logger.info(request.GET, request.POST)
-    return HttpResponse("Profiles app works.")
+def profiles_view(request):
+    if request.GET.get("parameter"):
+        logger.info(f"Parameter is: {request.GET.get('parameter')}")
+        return HttpResponse(f"Parameter is: {request.GET.get('parameter')}")
+    return HttpResponse(f"No parameters.")
