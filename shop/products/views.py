b42b714e72ad142a14a8898fa55d5fe5f595e882
Nick Markovicz <hh.markovich.n@gmail.com>
2023-02-23 13:38:32 +0300

    Fixed 'products_view' view, added 'variables' view

--- shop/products/views.py
+++ shop/products/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 import logging
 from django.http import HttpResponse
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 
 
-def products_index(request):
-    logger.info(request.GET, request.POST)
-    return HttpResponse("Products app works.")
+def index(request):
+    return HttpResponse(f"This is the Main Page.")
+
+
+def products_view(request):
+    if request.GET.get("parameter"):
+        logger.info(f"Parameter is: {request.GET.get('parameter')}")
+        return HttpResponse(f"Parameter is: {request.GET.get('parameter')}")
+    return HttpResponse(f"No parameters.")
+
+
+def variables(request):
+    logger.info(f"{settings.FIRST_VAR}")
+    if request.method == "GET":
+        if int(settings.FIRST_VAR) == 1:
+            return HttpResponse(f"{settings.SECOND_VAR}")
+        return HttpResponse(f"{settings.THIRD_VAR}")
