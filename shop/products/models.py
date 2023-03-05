1db9c343b1126bae3b4872847b0ab32e51718e28
Nick Markovicz <hh.markovich.n@gmail.com>
2023-03-05 19:05:34 +0300

    Added "color" field to Product model, added string representation to Product model, added string representation to Purchase model

--- shop/products/models.py
+++ shop/products/models.py
@@ -3,12 +3,18 @@ from django.db import models
 
 # Create your models here.
 
+COLOR_CHOICES = (("RED", "Red"), ("GREEN", "Green"), ("BLUE", "Blue"))
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
+    color = models.CharField(max_length=16, choices=COLOR_CHOICES, default="RED")
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return f"Product: {self.title} - {self.price}"
 
 
 class Purchase(models.Model):
@@ -20,3 +26,6 @@ class Purchase(models.Model):
     )
     count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return f"Purchase: {self.user} - {self.product} - {self.count}"
