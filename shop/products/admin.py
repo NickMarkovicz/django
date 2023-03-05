5b32e959802250ce1a80cb1ac89cc4f001f6e7ae
Nick Markovicz <hh.markovich.n@gmail.com>
2023-03-05 19:04:35 +0300

    Added Purchase inline into Product model

--- shop/products/admin.py
+++ shop/products/admin.py
@@ -4,12 +4,17 @@ from products.models import Product, Purchase
 # Register your models here.
 
 
+class PurchaseAdminInline(admin.TabularInline):
+    model = Purchase
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("title", "price", "created_at")
-    fields = ("title", "price", "description", "created_at")
+    list_display = ("title", "price", "color", "created_at")
+    fields = ("title", "price", "color", "description", "created_at")
     readonly_fields = ("created_at",)
-    search_fields = ("title", "price")
+    search_fields = ("title", "price", "color")
+    inlines = (PurchaseAdminInline,)
 
 
 @admin.register(Purchase)
@@ -17,4 +22,4 @@ class PurchaseAdmin(admin.ModelAdmin):
     list_display = ("user", "product", "count")
     fields = ("user", "product", "count", "created_at")
     readonly_fields = ("created_at",)
-    search_fields = ("user", "product")
+    search_fields = ("user__email", "product__title")
