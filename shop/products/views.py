f2935516ba08fc69565d4d42413a017310352476
Nick Markovicz <hh.markovich.n@gmail.com>
2023-03-05 19:06:16 +0300

    Added several filters, TRIED to add complex filters (count & price simultaneously)

--- shop/products/views.py
+++ shop/products/views.py
@@ -3,8 +3,10 @@ import logging
 from django.http import HttpResponse
 from django.conf import settings
 from products.models import Product
+from django.db.models import Sum, Count, Q
 
 logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 
@@ -14,11 +16,17 @@ def index(request):
 
 
 def products_view(request):
-    if request.GET.get("product"):
-        product = Product.objects.filter(title__contains=f"{request.GET.get('product')}").first()
-        return HttpResponse(f"""Title: {product.title},
-        Price: {product.price},
-        Description: {product.description}""")
     product_list = Product.objects.all()
-    return HttpResponse(f"""All products:
-{[product.title for product in product_list]}""")
+    if request.GET.get("title") is not None:
+        product_list = product_list.filter(title__icontains=request.GET.get("title"))
+    if request.GET.get("purchases_count") is not None:
+        product_list = product_list.filter(purchases__count=request.GET.get("purchases_count"))
+    if request.GET.get("price") is not None:
+        product_list = product_list.filter(product__price=request.GET.get("price"))
+    # if request.GET.get("purchases_revenue") is not None:  # Неправильно
+    #     price_list = [product.price for product in product_list]
+    #     count_list = [list(Product.objects.aggregate(count=Sum("purchases__count", filter=Q(price=price))).values()) for price in price_list]
+    #     count_list = sum(count_list, [])
+    #     purchases_revenue_list = [count_list[i] * price_list[i] for i in range(len(price_list))]
+    #     print(purchases_revenue_list)
+    return HttpResponse(f"{'<br>'.join([str(product) for product in product_list])}")
