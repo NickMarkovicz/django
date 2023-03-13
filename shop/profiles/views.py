0e3739d59761b8ad7cf5eb264f3856d57042a815
Nick Markovicz <hh.markovich.n@gmail.com>
2023-03-13 22:02:20 +0300

    Added "sign-up" function

--- shop/profiles/views.py
+++ shop/profiles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import logging
 from django.http import HttpResponse
 from django.conf import settings
+
+from profiles.forms import RegisterForm
 
 logger = logging.getLogger(__name__)
 
@@ -13,3 +15,19 @@ def profiles_view(request):
         logger.info(f"Parameter is: {request.GET.get('parameter')}")
         return HttpResponse(f"Parameter is: {request.GET.get('parameter')}")
     return HttpResponse(f"No parameters.")
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            logger.info(f"User Email: {form.cleaned_data['email']}")
+            logger.info(f"User password: {form.cleaned_data['password']}")
+            return redirect("/thanks/")
+    else:
+        form = RegisterForm()
+    return render(request, "sign_up.html", {"form": form})
+
+
+def thanks(request):
+    return HttpResponse(f"Thank you!")
